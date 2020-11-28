f999f63cdd6fdb72081251dbb94e1d27d9d3d50d
vrambo8 <vittotre@gmail.com>
2020-11-28 17:21:16 +0000

    Implemented batch norm after every block

--- pytorch_mlp_framework/train_evaluate_image_classification_system.py
+++ pytorch_mlp_framework/train_evaluate_image_classification_system.py
@@ -47,6 +47,9 @@ if args.block_type == 'conv_block':
 elif args.block_type == 'empty_block':
     processing_block_type = EmptyBlock
     dim_reduction_block_type = EmptyBlock
+elif args.block_type == 'bn_block':
+    processing_block_type = BatchNormProcessingBlock
+    dim_reduction_block_type = BatchNormDimensionalityReductionBlock
 else:
     raise ModuleNotFoundError
 
