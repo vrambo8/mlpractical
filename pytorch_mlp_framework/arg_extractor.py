4b17521c7109f7d7dc08028dc0b0dc1aa82baf9b
vrambo8 <vittotre@gmail.com>
2020-12-01 14:23:12 +0000

    Added learning rate parameter

--- pytorch_mlp_framework/arg_extractor.py
+++ pytorch_mlp_framework/arg_extractor.py
@@ -45,6 +45,8 @@ def get_args():
                         help='A flag indicating whether we will use GPU acceleration or not')
     parser.add_argument('--weight_decay_coefficient', nargs="?", type=float, default=0,
                         help='Weight decay to use for Adam')
+    parser.add_argument('--learning_rate', nargs="?", type=float, default=0.001,
+                        help='Learning rate to use for Adam')
     parser.add_argument('--block_type', type=str, default='conv_block',
                         help='Type of convolutional blocks to use in our network '
                              '(This argument will be useful in running experiments to debug your network)')
